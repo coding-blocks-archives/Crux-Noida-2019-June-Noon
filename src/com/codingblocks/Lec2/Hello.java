97e0a9aaaf392181c52a454c4ab7cda417d9d95b
shubham <shubhamrwt02@gmail.com>
2019-06-06 23:30:58 +0530

    Lec1 and Lec2

---
+++ src/com/codingblocks/Lec2/Hello.java
@@ -0,0 +1,19 @@
+package com.codingblocks.Lec2;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        int p=100;
+        int r=1;
+
+
+        if(p>r){
+            System.out.println("p is greater");
+        }
+        else{
+            System.out.println("r is greater");
+        }
+    }
+}
