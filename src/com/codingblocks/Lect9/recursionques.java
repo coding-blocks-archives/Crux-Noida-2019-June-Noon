ee797c5cb86dd72d7b0e1a8232f59eab00774b48
shubham <shubhamrwt02@gmail.com>
2019-06-17 21:45:52 +0530

    Lectures

--- src/com/codingblocks/Lect9/recursionques.java
+++ src/com/codingblocks/Lect9/recursionques.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class recursionques {
 
     public static void main(String[] args) {
-               dice("",4);
-          permutation("","ab");
+//               dice("",4);
+//          permutation("","ab");
 //
       //  System.out.println(replacepi("","pipipi}}pip"));
 //        ArrayList list=new ArrayList();
 //
 //       mazepath("",3,3);
-//        System.out.println(mazepathlist("",3,3,list));
+        System.out.println(mazepathlist1("",3,3));
     }
 
 
@@ -110,6 +110,31 @@ public class recursionques {
         if(col>1){
 
             mazepathlist(processed+'H',row,col-1,list);
+        }
+
+        return list;
+    }
+
+    public static ArrayList mazepathlist1(String processed,int row,int col){
+
+
+        if(row==1&&col==1){
+            ArrayList list =new ArrayList();
+
+            list.add(processed);
+            return list;
+        }
+
+        ArrayList list =new ArrayList();
+
+        if(row>1){
+
+            list.addAll(mazepathlist1(processed+'V',row-1,col));
+        }
+
+        if(col>1){
+
+          list.addAll(mazepathlist1(processed+'H',row,col-1));
         }
 
         return list;
