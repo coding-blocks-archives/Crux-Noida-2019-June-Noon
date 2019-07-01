3b8d57250b5bc6a973d698845259bc7a66c6433c
Keenwarrior <anujgargcse@gmail.com>
2019-07-01 14:48:33 +0530

    l 17

--- src/com/codingblocks/lecture_17/Client.java
+++ src/com/codingblocks/lecture_17/Client.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         GenericTree tree = new GenericTree();
-        Scanner scanner = new Scanner("10 true 1 false true 11 false false");
+        Scanner scanner = new Scanner("1 true 2 true 5 false true 6 false false true 3 true 7 false false true 4 false false");
         tree.populate(scanner);
 
         tree.display();
+
+        System.out.println("-----");
+
+        tree.preOrderItr();
     }
 }
--- src/com/codingblocks/lecture_17/GenericTree.java
+++ src/com/codingblocks/lecture_17/GenericTree.java
@@ -1,7 +1,9 @@
 package com.codingblocks.lecture_17;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class GenericTree {
 
@@ -42,6 +44,72 @@ public class GenericTree {
             display(node.children.get(i), indent + "\t");
         }
 
+    }
+
+    public boolean find(int target){
+        return find(root, target);
+    }
+
+    private boolean find(Node node, int target) {
+        boolean found = target == node.value;
+
+        for (int i = 0; i < node.children.size(); i++) {
+            found = found || find(node.children.get(i), target);
+        }
+
+        return found;
+    }
+
+    private int max(Node node) {
+        int max = node.value;
+
+        for (int i = 0; i < node.children.size(); i++) {
+            max = Math.max(max, max(node.children.get(i)));
+        }
+
+        return max;
+    }
+
+    private int maxWithChild(Node node) {
+        int max = node.value;
+        for (int i = 0; i < node.children.size(); i++) {
+            max += node.children.get(i).value;
+        }
+
+        for (int i = 0; i < node.children.size(); i++) {
+            max = Math.max(max, maxWithChild(node.children.get(i)));
+        }
+
+        return max;
+    }
+
+    public void preOrderItr(){
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            Node top = stack.pop();
+
+            System.out.println(top.value);
+            for (int i = top.children.size() - 1; i >= 0 ; i--) {
+                stack.push(top.children.get(i));
+            }
+        }
+    }
+
+    public int height(){
+        return height(root);
+    }
+
+    private int height(Node node) {
+
+        int max = 0;
+
+        for (int i = 0; i < node.children.size(); i++) {
+            max = Math.max(max, height(node.children.get(i)));
+        }
+
+        return max + 1;
     }
 
     private class Node {
